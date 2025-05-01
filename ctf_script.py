87d0d00436bc3055de4724362c9e0c67ea5e4fb3
Jon Narine <jon.wolf.narine@gmail.com>
2025-05-01 13:20:26 -0400

    fine tunes scraper and prints output

--- ctf_script.py
+++ ctf_script.py
@@ -7,18 +7,16 @@ driver.get("https://tns4lpgmziiypnxxzel5ss5nyu0nftol.lambda-url.us-east-1.on.aws
 
 chars = []
 
-sections = driver.find_elements(By.CSS_SELECTOR, 'section[data-id^="92]')
-
+sections = driver.find_elements(By.CSS_SELECTOR, 'section[data-id^="92"]')
 for section in sections:
   articles = section.find_elements(By.CSS_SELECTOR, 'article[data-class$="45"]')
   for article in articles:
     divs = article.find_elements(By.CSS_SELECTOR, 'div[data-tag*="78"]')
     for div in divs:
-      bs = b.find_elements(By.TAG_NAME, 'b')
+      bs = div.find_elements(By.TAG_NAME, 'b')
       for b in bs:
-        for b in bs:
-          class_attr = b.get_attribute("class")
-          if class_attr.strip() == "ref":
+          class_attr = b.get_attribute('class')
+          if "ref" in class_attr.strip():
             value = b.get_attribute("value")
             if value:
               chars.append(value)
@@ -26,4 +24,5 @@ for section in sections:
 hidden_url = ''.join(chars)
 print("url: ", hidden_url)
 
+input("Enter to exit")
 driver.quit()
